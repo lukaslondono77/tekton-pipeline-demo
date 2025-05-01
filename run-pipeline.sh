ebd00e98cc1d31e916dbd1251b513430d9bd7f17
Lukas Londono <152759905+lukaslondono77@users.noreply.github.com>
2025-05-01 11:59:55 -0500

    updated

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tkn pipeline start ci-pipeline \
-  -p repo-url=https://github.com/lukaslondono77/python-ci-demo.git \
+  -p repo-url=https://github.com/lukaslondono77/tekton-pipeline-demo.git \
   -p revision=main \
   -p IMAGE=my-local-image:latest \
   --workspace name=shared-data,volumeClaimTemplateFile=./pvc.yaml \
